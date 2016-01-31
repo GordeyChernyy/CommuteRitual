a0b2e240b189173be1c6f9bfc3244f6dcec2c97f
gordey <kewava@gmail.com>
2016-01-30 19:23:35 -0500

    Beat matching

--- Assets/BeatDetect.cs
+++ Assets/BeatDetect.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class BeatDetect : MonoBehaviour {
 	bool isRecord = false;
+	public AudioSource rythm;
 	int[] targetBeat;
 	int[] usersBeat;
 	public int bpm;
@@ -12,7 +13,9 @@ public class BeatDetect : MonoBehaviour {
 	int beatCount = 0;
 	public GameObject beatObject;
 	public GameObject canvas;
-	float canvasW = 800;
+	public GameObject arrow;
+	int buttonLap = 0;
+	float canvasW = 5;
 	ArrayList beatObjects = new ArrayList();
 
 	bool once = true;
@@ -20,7 +23,7 @@ public class BeatDetect : MonoBehaviour {
 		targetBeat = new int[500];
 		usersBeat = new int[500];
 	}
-	
+
 	void Start () {
 		StartCoroutine("Beat");
 		GameEngine.Instance.funcOnButtonBegin += SetRecord;
@@ -32,14 +35,20 @@ public class BeatDetect : MonoBehaviour {
 		secPerCount = 60.0f*bpmCount/bpm;
 	}
 	public void SetRecord(){
+		foreach(GameObject g in beatObjects){
+			Destroy(g);
+		}
 		isRecord = true;
 		print("isRecord" + isRecord);
 	}
 	public void CreateBeatObj(){
-		float size = canvasW*counter/secPerCount;
-		GameObject obj = Instantiate(beatObject, new Vector3(size, 0, 0) );
-		obj.transform.SetParent(canvas.transform);
-		beatObjects.Add(obj);
+		if(isRecord){
+			float size = Map(counter, 0, 85, 0, 20);
+			GameObject obj = Instantiate(beatObject);
+			obj.transform.SetParent(canvas.transform);
+			obj.transform.localPosition = new Vector3(size, 0, 0);
+			beatObjects.Add(obj);
+		}
 	}
 	// Update is called once per frame
 	private IEnumerator Beat()
@@ -48,17 +57,30 @@ public class BeatDetect : MonoBehaviour {
     	{
         	yield return new WaitForSeconds(secPerCount); // wait half a second
        		NewBeat();
+			rythm.Play ();
         	print("tunc!");
   		 }
 	}
+	public float Map( float value, float from1, float to1, float from2, float to2) {
+		return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+	}
 	void NewBeat(){
 		counter = 0;
-		isRecord = false;
+
+		if(buttonLap == 1 && isRecord){
+			buttonLap = 0;
+			isRecord = false;
+		}
+		if(isRecord){
+			buttonLap ++;
+		}
 		print(targetBeat);
 	}
 	void Update(){
 		print("counter = "+counter);
 		counter++;
+		float size = Map(counter, 0, 85, 0, 20);
+		arrow.transform.localPosition = new Vector3 (size, 0, 0);
 		if(isRecord){
 			if(GameEngine.Instance.isTouch){
 				targetBeat[counter] = 1;
